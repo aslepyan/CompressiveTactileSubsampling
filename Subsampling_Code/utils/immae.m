f10df708735d831f6a3647578665422cbf5745d9
dianlics <127831779+dianlics@users.noreply.github.com>
2024-11-01 01:07:50 -0400

    Delete Subsampling_Code directory

--- Subsampling_Code/utils/immae.m
+++
@@ -1,9 +0,0 @@
-function err=immae(im1,im2)
-% function for calculating the mae between 2 images
-% input:
-% im1 - original image
-% im2 - deformed image
-% output:
-% acc - accuracy
-
-err=sum(abs(im1(:)-im2(:)))/numel(im1);
